a4dd12266887f2eb49a813646c46cfc58e0a3a0f
somgr <deniska9767@gmail.com>
2023-05-01 16:47:21 +0500

    add unit test for article model

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from blog.views import home_page
+from blog.models import Article
 from django.http import HttpRequest
 
 class HomePageTest(TestCase):
@@ -18,3 +19,39 @@ class HomePageTest(TestCase):
         self.assertIn('<h1>Денис</h1>', html)
         self.assertTrue(html.endswith('</html>'))
 
+
+class ArticleModelTest(TestCase):
+    
+    def test_article_model_save_and_retrieve(self):
+        article1 = Article(
+            title='article 1',
+            full_text='full_text',
+            summary='summary 1',
+            category='category 1',
+            pubdate='pubdate 1',
+        )
+        article1.save()
+        
+        article2 = Article(
+            title='article 2',
+            full_text='full_text',
+            summary='summary 2',
+            category='category 2',
+            pubdate='pubdate 2',
+        )
+        
+        article2.save()
+
+        all_articles = Article.objects.all()
+        
+        self.assertEqual(len(all_articles), 2)
+
+        self.assertEqual(
+            all_articles[0].title,
+            article1.title
+        )
+
+        self.assertEqual(
+            all_articles[1].title,
+            article2.title
+        )
